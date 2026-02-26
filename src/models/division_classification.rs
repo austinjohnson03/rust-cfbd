e01394acd2eafb0eeebca86664342866ffd0126a
austinjohnson03 <amj102998@gmail.com>
2026-02-26 11:49:17 -0500

    added division class & season type enums
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

---
+++ src/models/division_classification.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum DivisionClassification {
+    Fbs,
+    Fcs,
+    #[serde(rename = "ii")]
+    Div2,
+    #[serde(rename = "iii")]
+    Div3,
+}
+
+impl fmt::Display for DivisionClassification {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            DivisionClassification::Fbs => "FBS",
+            DivisionClassification::Fcs => "FCS",
+            DivisionClassification::Div2 => "II",
+            DivisionClassification::Div3 => "III",
+        };
+
+        write!(f, "{}", s)
+    }
+}
+
+impl DivisionClassification {
+    pub fn is_division_1(&self) -> bool {
+        match self {
+            DivisionClassification::Fbs | DivisionClassification::Fcs => true,
+            _ => false,
+        }
+    }
+}
---
+++ src/models/season_type.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum SeasonType {
+    Regular,
+    Postseason,
+    Both,
+    Allstar,
+    #[serde(rename = "spring_regular")]
+    SpringRegular,
+    #[serde(rename = "spring_postseason")]
+    SpringPostseason,
+}
+
+impl fmt::Display for SeasonType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            SeasonType::Regular => "Regular",
+            SeasonType::Postseason => "Postseason",
+            SeasonType::Both => "Both",
+            SeasonType::Allstar => "Allstar",
+            SeasonType::SpringRegular => "Spring Regular",
+            SeasonType::SpringPostseason => "Spring Postseason",
+        };
+
+        write!(f, "{}", s)
+    }
+}
