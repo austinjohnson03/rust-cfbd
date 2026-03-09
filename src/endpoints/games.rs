ce1e0a80bf8919358b45b8d4792279b0e0b042c5
austinjohnson03 <amj102998@gmail.com>
2026-03-09 17:26:19 -0400

    finished game media endpoint
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/endpoints/games.rs
+++ src/endpoints/games.rs
@@ -1,6 +1,8 @@
 use crate::client::CfbdClient;
 use crate::error::CFBDError;
 use crate::models::cfb::game::Game;
+use crate::models::cfb::game_media::GameMedia;
+use crate::models::cfb::game_media_params::GameMediaQuery;
 use crate::models::cfb::get_game_params::{ById, ByYear, GetGameParams};
 use serde::Serialize;
 
@@ -11,5 +13,12 @@ impl GameQuery for GetGameParams<ByYear> {}
 impl CfbdClient {
     pub async fn get_games<P: GameQuery>(&self, params: &P) -> Result<Vec<Game>, CFBDError> {
         self.get("games", Some(params)).await
+    }
+
+    pub async fn get_game_media(
+        &self,
+        params: &GameMediaQuery,
+    ) -> Result<Vec<GameMedia>, CFBDError> {
+        self.get("games/media", Some(params)).await
     }
 }
---
+++ src/models/cfb/game_media_params.rs
@@ -0,0 +1,126 @@
+use crate::models::cfb::division_classification::DivisionClassification;
+use crate::models::cfb::media_type::MediaType;
+use crate::models::cfb::season_type::SeasonType;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+
+pub struct NoQuery;
+pub struct ByYear;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GameMediaQuery {
+    pub year: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub season_type: Option<SeasonType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub week: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub team: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub conference: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub media_type: Option<MediaType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub classification: Option<DivisionClassification>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct GameMediaParams<Q> {
+    #[serde(skip)]
+    _query: PhantomData<Q>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub year: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub season_type: Option<SeasonType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub week: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub team: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub conference: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub media_type: Option<MediaType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub classification: Option<DivisionClassification>,
+}
+
+impl GameMediaParams<NoQuery> {
+    pub fn new() -> Self {
+        GameMediaParams {
+            _query: PhantomData,
+            year: None,
+            season_type: None,
+            week: None,
+            team: None,
+            conference: None,
+            media_type: None,
+            classification: None,
+        }
+    }
+
+    pub fn year(self, year: i32) -> GameMediaParams<ByYear> {
+        GameMediaParams {
+            _query: PhantomData,
+            year: Some(year),
+            season_type: self.season_type,
+            week: self.week,
+            team: self.team,
+            conference: self.conference,
+            media_type: self.media_type,
+            classification: self.classification,
+        }
+    }
+}
+
+impl Default for GameMediaParams<NoQuery> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl GameMediaParams<ByYear> {
+    pub fn build(self) -> GameMediaQuery {
+        GameMediaQuery {
+            year: self.year.unwrap(),
+            season_type: self.season_type,
+            week: self.week,
+            team: self.team,
+            conference: self.conference,
+            media_type: self.media_type,
+            classification: self.classification,
+        }
+    }
+}
+
+impl<Q> GameMediaParams<Q> {
+    pub fn season_type(mut self, season_type: SeasonType) -> Self {
+        self.season_type = Some(season_type);
+        self
+    }
+
+    pub fn week(mut self, week: i32) -> Self {
+        self.week = Some(week);
+        self
+    }
+
+    pub fn team(mut self, team: String) -> Self {
+        self.team = Some(team);
+        self
+    }
+
+    pub fn conference(mut self, conference: String) -> Self {
+        self.conference = Some(conference);
+        self
+    }
+
+    pub fn media_type(mut self, media_type: MediaType) -> Self {
+        self.media_type = Some(media_type);
+        self
+    }
+
+    pub fn classification(mut self, classification: DivisionClassification) -> Self {
+        self.classification = Some(classification);
+        self
+    }
+}
