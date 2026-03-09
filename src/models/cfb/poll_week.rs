8ee6ea89afc9e0a9dcfffe31216b03fc150b2e4e
austinjohnson03 <amj102998@gmail.com>
2026-03-09 01:58:14 -0400

    added poll week model
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

---
+++ src/models/cfb/poll_week.rs
@@ -0,0 +1,29 @@
+use crate::models::cfb::season_type::SeasonType;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PollRank {
+    pub rank: Option<i32>,
+    pub team_id: i32,
+    pub school: String,
+    pub confernce: Option<String>,
+    pub first_place_votes: Option<i32>,
+    pub points: Option<i32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Poll {
+    pub poll: String,
+    pub ranks: Vec<PollRank>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PollWeek {
+    pub season: i32,
+    pub season_type: SeasonType,
+    pub week: i32,
+    pub polls: Vec<Poll>,
+}
