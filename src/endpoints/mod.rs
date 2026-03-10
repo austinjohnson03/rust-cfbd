b698232990eacf79245c242b8b0f6ab2ec1c3613
austinjohnson03 <amj102998@gmail.com>
2026-03-10 10:39:35 -0400

    finsihed team struct and endpoint
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -1,3 +1,4 @@
 pub mod conference;
 pub mod games;
 pub mod info;
+pub mod teams;
--- src/models/cfb/team_params.rs
+++ src/models/cfb/team_params.rs
@@ -1,1 +1,29 @@
+use serde::Serialize;
 
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TeamQuery {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    conference: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    year: Option<i32>,
+}
+
+impl TeamQuery {
+    pub fn new() -> Self {
+        TeamQuery {
+            conference: None,
+            year: None,
+        }
+    }
+
+    pub fn conference(mut self, conference: String) -> Self {
+        self.conference = Some(conference);
+        self
+    }
+
+    pub fn year(mut self, year: i32) -> Self {
+        self.year = Some(year);
+        self
+    }
+}
