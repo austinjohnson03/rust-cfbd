e817b923b50ced1727a5fbf1f5bc90c6dc8f09c6
austinjohnson03 <amj102998@gmail.com>
2026-03-25 14:58:18 -0400

    added partial eq to structs
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/models/cfb/entity/media_type.rs
+++ src/models/cfb/entity/media_type.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum MediaType {
     Tv,
