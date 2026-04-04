dbbcc8af1301d509e98118bb878e0c3832583946
austinjohnson03 <amj102998@gmail.com>
2026-04-04 15:07:26 -0400

    added IntoOptional template trait
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/common/conversion.rs
+++ src/common/conversion.rs
@@ -25,3 +25,19 @@ impl IntoOptionalString for Option<&str> {
         self.map(|s| s.to_string())
     }
 }
+
+pub trait IntoOptional<T> {
+    fn into_optional(self) -> Option<T>;
+}
+
+impl<T> IntoOptional<T> for T {
+    fn into_optional(self) -> Option<T> {
+        Some(self)
+    }
+}
+
+impl<T> IntoOptional<T> for Option<T> {
+    fn into_optional(self) -> Option<T> {
+        self
+    }
+}
