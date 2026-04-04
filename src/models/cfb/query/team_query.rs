49932cdcd1cc511dfd844637e28d4e6d2f49e0d0
austinjohnson03 <amj102998@gmail.com>
2026-04-04 14:29:05 -0400

    updated query builder methods to allow option
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/models/cfb/query/team_query.rs
+++ src/models/cfb/query/team_query.rs
@@ -1,3 +1,7 @@
+use crate::{
+    common::conversion::IntoOptionalString,
+    models::cfb::entity::division_classification::DivisionClassification,
+};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -7,11 +11,14 @@ pub struct TeamQuery {
     conference: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     year: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    classification: Option<DivisionClassification>,
 }
 
 pub struct TeamQueryBuilder {
     conference: Option<String>,
     year: Option<i32>,
+    classification: Option<DivisionClassification>,
 }
 
 impl Default for TeamQueryBuilder {
@@ -25,16 +32,25 @@ impl TeamQueryBuilder {
         TeamQueryBuilder {
             conference: None,
             year: None,
+            classification: None,
         }
     }
 
-    pub fn conference(mut self, conference: impl Into<String>) -> Self {
-        self.conference = Some(conference.into());
+    pub fn conference(mut self, conference: impl IntoOptionalString) -> Self {
+        self.conference = conference.into_optional_string();
         self
     }
 
-    pub fn year(mut self, year: i32) -> Self {
-        self.year = Some(year);
+    pub fn year(mut self, year: impl Into<Option<i32>>) -> Self {
+        self.year = year.into();
+        self
+    }
+
+    pub fn classification(
+        mut self,
+        classification: impl Into<Option<DivisionClassification>>,
+    ) -> Self {
+        self.classification = classification.into();
         self
     }
 
@@ -42,6 +58,7 @@ impl TeamQueryBuilder {
         TeamQuery {
             conference: self.conference,
             year: self.year,
+            classification: self.classification,
         }
     }
 }
