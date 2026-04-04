c361f421f251af571f52cee276c7ba8bfd57bb78
austinjohnson03 <amj102998@gmail.com>
2026-04-04 08:43:03 -0400

    updated division classification with several methods
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/models/cfb/entity/division_classification.rs
+++ src/models/cfb/entity/division_classification.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
@@ -28,5 +29,50 @@ impl DivisionClassification {
             self,
             DivisionClassification::Fbs | DivisionClassification::Fcs
         )
+    }
+
+    pub fn is_fbs(&self) -> bool {
+        matches!(self, DivisionClassification::Fbs)
+    }
+
+    pub fn all() -> Vec<DivisionClassification> {
+        vec![
+            DivisionClassification::Fbs,
+            DivisionClassification::Fcs,
+            DivisionClassification::Div2,
+            DivisionClassification::Div3,
+        ]
+    }
+
+    pub fn label(&self) -> &str {
+        match self {
+            DivisionClassification::Fbs => "FBS",
+            DivisionClassification::Fcs => "FCS",
+            DivisionClassification::Div2 => "II",
+            DivisionClassification::Div3 => "III",
+        }
+    }
+
+    pub fn as_str(&self) -> &str {
+        match self {
+            DivisionClassification::Fbs => "fbs",
+            DivisionClassification::Fcs => "fcs",
+            DivisionClassification::Div2 => "ii",
+            DivisionClassification::Div3 => "iii",
+        }
+    }
+}
+
+impl FromStr for DivisionClassification {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "fbs" => Ok(DivisionClassification::Fbs),
+            "fcs" => Ok(DivisionClassification::Fcs),
+            "ii" => Ok(DivisionClassification::Div2),
+            "iii" => Ok(DivisionClassification::Div3),
+            _ => Err(format!("Invalid division classification: {}", s)),
+        }
     }
 }
