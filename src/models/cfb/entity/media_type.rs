6a8c4f99c0e97e546eedab54bce12dd58dce7ad3
austinjohnson03 <amj102998@gmail.com>
2026-04-28 04:24:29 -0400

    added FromStr method for media type enum
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/models/cfb/entity/media_type.rs
+++ src/models/cfb/entity/media_type.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
@@ -8,4 +9,19 @@ pub enum MediaType {
     Web,
     Ppv,
     Mobile,
+}
+
+impl FromStr for MediaType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "tv" => Ok(MediaType::Tv),
+            "radio" => Ok(MediaType::Radio),
+            "web" => Ok(MediaType::Web),
+            "ppv" => Ok(MediaType::Ppv),
+            "mobile" => Ok(MediaType::Mobile),
+            _ => Err(format!("Invalid media type: {}", s)),
+        }
+    }
 }
