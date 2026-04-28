d6b0b8cb077809665605d34b38daf8d2a77ebdde
austinjohnson03 <amj102998@gmail.com>
2026-04-28 04:20:24 -0400

    added FromStr method for season type enum
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/models/cfb/entity/season_type.rs
+++ src/models/cfb/entity/season_type.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
@@ -26,5 +27,21 @@ impl fmt::Display for SeasonType {
         };
 
         write!(f, "{}", s)
+    }
+}
+
+impl FromStr for SeasonType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "regular" => Ok(SeasonType::Regular),
+            "postseason" => Ok(SeasonType::Postseason),
+            "both" => Ok(SeasonType::Both),
+            "allstar" => Ok(SeasonType::Allstar),
+            "spring regular" => Ok(SeasonType::SpringRegular),
+            "spring postseason" => Ok(SeasonType::SpringPostseason),
+            _ => Err(format!("Invalid season type: {}", s)),
+        }
     }
 }
